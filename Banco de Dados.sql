a8b2cd5d922b085577f5b47bcc021ab948a5bf5c
Bruno <brunoagra988@gmail.com>
2024-12-08 13:19:23 -0300

    Atualizar o Banco de Dados.sql

--- Banco de Dados.sql
+++ Banco de Dados.sql
@@ -6,7 +6,7 @@ CREATE TABLE usuarios (
     nome VARCHAR(40) NOT NULL,
     usuario VARCHAR(40) NOT NULL,
     senha VARCHAR(40) NOT NULL,
-	email VARCHAR(40) NOT NULL,
+	   email VARCHAR(40) NOT NULL,
     PRIMARY KEY (id)
 );
 GO
